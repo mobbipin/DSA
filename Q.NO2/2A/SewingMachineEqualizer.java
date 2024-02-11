1572e164f6e47e8ac42a1e4c2152430c0622700f
mobbbipin <mobbipin@mobcr3atives.com>
2024-02-12 04:13:24 +0545

    QNO2A

---
+++ Q.NO2/2A/SewingMachineEqualizer.java
@@ -0,0 +1,49 @@
+public class SewingMachineEqualizer {
+
+    public static int[] minMovesToEqualize(int[] dresses) {
+        int totalDresses = 0;
+        int totalMachines = dresses.length;
+
+      
+        for (int dressCount : dresses) {
+            totalDresses += dressCount;
+        }
+
+    
+        int targetDressesPerMachine = totalDresses / totalMachines;
+        if (totalDresses % totalMachines != 0) {
+            return new int[]{-1, -1}; 
+        }
+
+        int currentMoveCount = 0;
+
+      
+        for (int i = 0; i < dresses.length; i++) {
+            while (dresses[i] > targetDressesPerMachine) {
+            
+                for (int j = 0; j < dresses.length; j++) {
+                    if (dresses[j] < targetDressesPerMachine) {
+                        int dressesToMove = Math.min(targetDressesPerMachine - dresses[j], dresses[i] - targetDressesPerMachine);
+                        dresses[i] -= dressesToMove;
+                        dresses[j] += dressesToMove;
+                        currentMoveCount += dressesToMove;
+                    }
+                }
+            }
+        }
+
+
+        return new int[]{currentMoveCount, currentMoveCount};
+    }
+
+    public static void main(String[] args) {
+        int[] dresses = {2, 1, 3, 0, 2};
+        int[] result = minMovesToEqualize(dresses);
+
+        if (result[0] == -1) {
+            System.out.println("Cannot be equalized");
+        } else {
+            System.out.println("Number of moves: " + result[0]);
+        }
+    }
+}
